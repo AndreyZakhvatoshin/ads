8581b1847cb57618b0882c947cecc2932c4fffd2
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-05-31 22:32:51 +0500

    add rules validation

--- app/Http/Requests/Admin/Users/CreateUserRequest.php
+++ app/Http/Requests/Admin/Users/CreateUserRequest.php
@@ -24,7 +24,8 @@ class CreateUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users'
         ];
     }
 }
