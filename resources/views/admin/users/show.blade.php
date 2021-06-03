f02b5764ddb22a9921d7abb0c50525117e32c43e
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-06-03 22:47:53 +0500

    add verify link

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -4,6 +4,12 @@
         @include('admin.users._nav')
         <div class="d-flex flex-row mb-3 py-2">
             <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-primary mr-1">Редактировать</a>
+            @if (!$user->email_verified_at)
+                <form action="{{ route('admin.users.verify', $user) }}" method="POST">
+                    @csrf
+                    <button class="btn btn-warning mr-1">Подтвердить email</button>
+                </form>
+            @endif
             <form action="{{ route('admin.users.destroy', $user) }}" method="POST">
                 @csrf
                 @method('DELETE')
@@ -28,6 +34,16 @@
                         <span class="badge badge-danger">Admin</span>
                     @else
                         <span class="badge badge-secondary">User</span>
+                    @endif
+                </td>
+            </tr>
+            <tr>
+                <th>Подтвержден email</th>
+                <td>
+                    @if ($user->isVerify())
+                        <span class="badge badge-secondary">Да</span>
+                    @else
+                    <span class="badge badge-danger">Нет</span>
                     @endif
                 </td>
             </tr>
