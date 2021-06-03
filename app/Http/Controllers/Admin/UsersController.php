cc48fffce7715e9397f5b538a13c006c8c6e199b
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-06-03 18:14:39 +0500

    add update request

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Users\CreateUserRequest;
 use App\Http\Requests\Admin\Users\UpdateUserRequest;
 use App\Models\User;
 use Carbon\Carbon;
@@ -41,7 +42,7 @@ class UsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateUserRequest $request)
     {
         $user = User::new($request['name'], $request['email']);
         return redirect()->route('admin.users.index')->with('success', 'Пользователь добавлен');
