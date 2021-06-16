cd62e018da3b848e85f8111ff41f8acb6cfadaae
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-06-16 23:24:28 +0500

    add fields

--- database/migrations/2021_06_16_175445_create_cities_table.php
+++ database/migrations/2021_06_16_175445_create_cities_table.php
@@ -14,8 +14,8 @@ class CreateCitiesTable extends Migration
     public function up()
     {
         Schema::create('cities', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+            $table->string('name')->index();
         });
     }
 
