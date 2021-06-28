80d2b371480650314029194505ad8aa725912e21
Andrey Zakhvatoshin <mastetandron@gmail.com>
2021-06-28 22:41:03 +0500

    add regions link

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -10,6 +10,9 @@
                 <li class="nav-item">
                   <a class="nav-link" href="{{ route('admin.users.index') }}">Пользователи</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.regions.index') }}">Регионы</a>
+                </li>
             </ul>
         </div>
     </div>
